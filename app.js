afdeba7fae94b26b4310d6ea33d424b8ef771bd2
Juanfle <juanfleming2006@gmail.com>
2025-10-08 13:50:55 -0300

    borre la pregunta

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-
-// Actividad DOM
-// Crear un título h1 de bienvenida para la página, además de un párrafo p con la lista de los 
-// elementos a comprar, enumerados.
-// Crear un input de texto donde el usuario pueda escribir el nombre o número del producto que 
-// quiere comprar. Agregar un botón que diga “Comprar”. Debajo del botón colocar un párrafo 
-// vacío p donde se va a mostrar el precio del producto elegido.
-// Al hacer click en el botón:
-// Capturar lo que se escribió en el input, mostrar en la consola el producto que se eligió y 
-// luego en el párrafo mostrar el precio correspondiente del producto. Si el producto no existe, 
-// o está mal escrito, mostrar “Producto no encontrado”.
-// Adicional, puede agregarse otro input que recibe un número de productos, y el precio final 
-// que se muestre sea el total de todos los productos (un solo producto varias veces)
-
 let boton = document.getElementById("comprarButton");
 let inputProducto = document.getElementById("productoInput");
 let inputCantidad = document.getElementById("cantidadInput");
